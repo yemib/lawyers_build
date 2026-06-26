702044723e1d03e88cb19882ba8e41a9ebd45db0
Obafemi Emmanuel <obafemie@gmail.com>
2026-06-26 13:43:57 +0100

    upload the content

--- sw.js
+++ sw.js
@@ -1,5 +1,36 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+const CACHE_NAME = "lawyers-connect-v1";
+
+const urlsToCache = [
+  "/",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  console.log("Service Worker Installed");
+
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (event) => {});
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker Activated");
+
+  event.waitUntil(
+    self.clients.claim()
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
